7da741e297c20853bfee83f759467c8641dc05af
Andrew Armenia <armena@ultradian.com>
2011-08-11 14:50:28 -0400

    add command input parser

--- led_sign.c
+++ led_sign.c
@@ -23,10 +23,43 @@ static void draw_clock(struct clock *clock) {
     set_digit(9, clock->ten_days);
 }
 
+static void execute_cmd(struct clock *clock, int where, int ch) {
+    if (ch < '0' || ch > '9') {
+        return;
+    }
+
+    int8_t new_hours;
+    int8_t digit = ch - '0';
+
+    switch (where) {
+        case 'D': clock->ten_days = digit; break;
+        case 'd': clock->days = digit; break;
+        case 'H':
+            new_hours = digit * 10 + clock->hours;
+            if (new_hours < 24) {
+                clock->ten_hours = digit;
+            }
+            break;
+        case 'h':
+            new_hours = 10 * clock->ten_hours + digit;
+            if (new_hours < 24) {
+                clock->hours = digit;
+            }
+            break;
+        case 'M': if (digit < 6) clock->ten_minutes = digit; break;
+        case 'm': clock->minutes = digit; break;
+        case 'S': if (digit < 6) clock->ten_seconds = digit; break;
+        case 's': clock->seconds = digit; break;
+        case 'T': clock->tenths = digit; break;
+        case 't': clock->hundredths = digit; break;
+    }
+}
+
 int main(void) {
-    uint8_t i;
-    uint8_t flag = 0;
+    uint8_t flag = 1;
     int16_t horn_timeout = 0;
+    int ch;
+    int where = 0;
 
     struct clock clock;
 
@@ -40,12 +73,50 @@ int main(void) {
     sei( );
 
     memset(&clock, 0, sizeof(struct clock));
-    clock.ten_seconds = 1;
 
     for (;;) {
         if (usb_serial_available( ) > 0) {
             /* process input */
-            usb_serial_flush_input( ); /* whoops */
+            ch = usb_serial_getchar( );
+            switch (ch) {
+                case 'p': /* pause */
+                    flag = 1;
+                    horn_timeout = 0;
+                    break;
+
+                case 'g': /* go */
+                    flag = 0;
+                    break;
+    
+                case 'D': /* 10-days */
+                case 'd': /* days */
+                case 'H': /* 10-hours */
+                case 'h': /* hours */
+                case 'M': /* 10-minutes */
+                case 'm': /* minutes */
+                case 'S': /* 10-seconds */
+                case 's': /* seconds */
+                case 'T': /* tenths */
+                case 't': /* hundredths (tenths of tenths) */
+                    where = ch;
+                    break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    /* only execute commands when paused */
+                    if (!flag) {
+                        execute_cmd(&clock, where, ch);
+                    }
+                    break;
+            }
         } else {
             if (!flag) {
                 if (update_clock(&clock, main_timer_counts( )) == 0) {
